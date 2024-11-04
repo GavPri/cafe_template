b3ef1e6fc9333261396c0d445f64b36948dcce7c
德华 刘 <gavinprior5@gmail.com>
2024-11-04 16:20:33 +0000

    add hamburger icon

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -7,11 +7,32 @@ const NavigationBar = () => {
     setMenuOpen(!menuOpen);
   };
   return (
-    <div className="w-full h-24 bg-bg flex justify-between items-center px-4">
+    <div className="w-full h-24 bg-bg flex justify-between items-center px-6">
       <h1 className="text-textP text-2xl tracking-wider">Paramount Cafe</h1>
-      <div className="flex flex-col" onClick={handleMenuChange}>
-        <span className="w-6 h-0.5 bg-textS rounded-md inline-block mb-2"></span>
-        <span className="w-6 h-0.5 bg-textS rounded-md inline-block"></span>
+      <div
+        className="flex flex-col relative h-4 mr-6"
+        onClick={handleMenuChange}
+      >
+        <span
+          className={`w-6 h-0.5 bg-textS rounded-md inline-block mb-0 transition-transform duration-200 absolute top-0 ${
+            menuOpen ? "w-0" : ""
+          }`}
+        ></span>
+        <span
+          className={`w-6 h-0.5 bg-textS rounded-md inline-block mb-0 transition-transform duration-200 absolute top-1/2 ${
+            menuOpen ? "-rotate-45 " : ""
+          }`}
+        ></span>
+        <span
+          className={`w-6 h-0.5 bg-textS rounded-md inline-block mb-0 transition-transform duration-200 absolute top-1/2 ${
+            menuOpen ? "rotate-45 " : ""
+          }`}
+        ></span>
+        <span
+          className={`w-6 h-0.5 bg-textS rounded-md inline-block transition-transform duration-200 absolute top-[100%] ${
+            menuOpen ? " w-0" : ""
+          }`}
+        ></span>
       </div>
     </div>
   );
