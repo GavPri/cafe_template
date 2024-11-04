bdd6d74a947fe7b5cc36dd113e6752b1c3589fb4
德华 刘 <gavinprior5@gmail.com>
2024-11-04 16:06:01 +0000

    ad boilerplate navigation bar

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+const NavigationBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  
+  return (
+    <div className="w-full h-24 bg-bg flex justify-between items-center px-4">
+      <h1 className="text-textP text-2xl tracking-wider">Paramount Cafe</h1>
+      <div className="flex flex-col">
+        <span className="w-6 h-0.5 bg-textS rounded-md inline-block mb-2"></span>
+        <span className="w-6 h-0.5 bg-textS rounded-md inline-block"></span>
+      </div>
+    </div>
+  );
+};
+
+export default NavigationBar;
