940feb1424f31ddc54212cc6e233e08691f409f3
德华 刘 <gavinprior5@gmail.com>
2024-11-05 17:56:29 +0000

    create footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import { MdCopyright } from "react-icons/md";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="mt-auto w-full h-12 bg-bg border-t-2 border-bordera">
+      <p className="text-textS flex items-center justify-end mr-2 mt-3 text-xs font-thin">
+        <MdCopyright size={20} /> Paramount Cafe. All rights reserved 2024
+      </p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
