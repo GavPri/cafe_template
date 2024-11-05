453ac4d5aaa6b66c47b4dd799450e422d83b5bd3
德华 刘 <gavinprior5@gmail.com>
2024-11-05 17:57:03 +0000

    add footer to root

--- src/routing/router.jsx
+++ src/routing/router.jsx
@@ -2,12 +2,13 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-  Outlet
+  Outlet,
 } from "react-router-dom";
 import NavigationBar from "../components/NavigationBar";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import FindUs from "../pages/FindUs";
+import Footer from "../components/Footer";
 
 // Root Component
 const Root = () => {
@@ -19,7 +20,7 @@ const Root = () => {
       <div className="min-h-[calc(100vh-96px)] flex flex-col">
         <Outlet />
         {/* TODO --- add Footer */}
-        {/* <Footer /> */}
+        <Footer />
       </div>
     </>
   );
