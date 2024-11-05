4020a42a6ce2249676c436bae74107483a6ba51c
德华 刘 <gavinprior5@gmail.com>
2024-11-05 13:25:08 +0000

    adjust posotioning of navbar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import HamburgerMenu from "./HamburgerMenu";
+import MobileMenu from "./MobileMenu";
 
 const NavigationBar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -8,10 +9,21 @@ const NavigationBar = () => {
     setMenuOpen(!menuOpen);
   };
   return (
-    <div className="w-full h-24 bg-bg flex justify-between items-center px-6">
-      <h1 className="text-textP text-2xl tracking-wider">Paramount Cafe</h1>
-      <HamburgerMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} handleMenuChange={handleMenuChange} />
-    </div>
+    <>
+      <div className="w-full h-24 bg-bg flex justify-between items-center px-6">
+        <h1 className="text-textP text-2xl tracking-wider z-10">Paramount Cafe</h1>
+        <HamburgerMenu
+          menuOpen={menuOpen}
+          setMenuOpen={setMenuOpen}
+          handleMenuChange={handleMenuChange}
+        />
+      </div>
+      <MobileMenu
+        menuOpen={menuOpen}
+        setMenuOpen={setMenuOpen}
+        handleMenuChange={handleMenuChange}
+      />
+    </>
   );
 };
 
