d075c097d7149cf46350ace4702763416f20e4be
德华 刘 <gavinprior5@gmail.com>
2024-11-05 17:27:52 +0000

    adjust import for router

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,8 @@
-import NavigationBar from "./components/NavigationBar";
-// * imports for app routing
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  Outlet,
-  RouterProvider,
-} from "react-router-dom";
-
-import Home from "../src/pages/Home";
-import About from "../src/pages/About";
-import FindUs from "../src/pages/FindUs";
+// App.js
+import { RouterProvider } from "react-router-dom";
+import router from "../src/routing/router";
 
 function App() {
-  // Root Variable
-  const Root = () => {
-    return (
-      <>
-        <div>
-          <NavigationBar />
-        </div>
-        <div className="min-h-screen">
-          <Outlet />
-          {/* TODO --- add Footer */}
-          {/* <Footer/>  */}
-        </div>
-      </>
-    );
-  };
-
-  // router variable
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<Root />}>
-        <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="find-us" element={<FindUs />} />
-      </Route>
-    )
-  );
   return <RouterProvider router={router} />;
 }
 
