58c2146f4e244715aedf1b9dce90c2f28e994308
德华 刘 <gavinprior5@gmail.com>
2024-11-05 16:57:57 +0000

    add imports for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,12 @@
 import NavigationBar from "./components/NavigationBar";
+// * imports for app routing
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
   return (
