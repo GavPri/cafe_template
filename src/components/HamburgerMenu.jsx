42f2c656d9d25ba508d86cdf6a68085460ef172b
德华 刘 <gavinprior5@gmail.com>
2024-11-05 13:23:45 +0000

    add spans for animation

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const HamburgerMenu = ({menuOpen, handleMenuChange}) => {
+const HamburgerMenu = ({ menuOpen, handleMenuChange }) => {
   return (
-    <div className="flex flex-col relative h-4 mr-6 text-textS" onClick={handleMenuChange}>
+    <div
+      className="flex flex-col relative h-4 mr-6 text-textS z-10"
+      onClick={handleMenuChange}
+    >
       <span
-        className={`w-6 h-0.5 bg-textS rounded-md inline-block mb-0 transition-transform duration-200 absolute top-0 ${
-          menuOpen ? "w-0" : ""
+        className={` h-0.5 bg-textS rounded-md inline-block mb-0 transition-transform duration-200 absolute top-0 ${
+          menuOpen ? "w-0" : "w-6"
         }`}
       ></span>
       <span
@@ -19,12 +22,12 @@ const HamburgerMenu = ({menuOpen, handleMenuChange}) => {
         }`}
       ></span>
       <span
-        className={`w-6 h-0.5 bg-textS rounded-md inline-block transition-transform duration-200 absolute top-[100%] ${
-          menuOpen ? " w-0" : ""
+        className={`h-0.5 bg-textS rounded-md inline-block transition-transform duration-200 absolute top-[100%] ${
+          menuOpen ? " w-0" : "w-6"
         }`}
       ></span>
     </div>
   );
-}
+};
 
-export default HamburgerMenu
+export default HamburgerMenu;
